990b3e062081c30709e35ffb8a31e49557538742
MarkWaldron <markewaldron@gmail.com>
2015-05-20 20:40:04 -0400

    Add Article show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,41 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :destroy]
+
+  def index
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
 end
